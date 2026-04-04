f47e67795595e40285d62f5eb30c7c8b30135f6b
Zoey Chen <zixiu.zoey.chen@gmail.com>
2026-04-04 19:04:34 -0400

    spring boot is good (h2 db in gradle)

---
+++ concept/src/main/java/tbr/concept/ConceptApplication.java
@@ -0,0 +1,13 @@
+package tbr.concept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConceptApplication.class, args);
+    }
+
+}
